8d3c102b65514591da8f3e9fb6796917515eea5b
pankypan <1356523334@qq.com>
2020-10-29 23:24:54 +0800

    '单调栈'

--- leetcode/basic02_stack_and_queue/s04_min_stack.py
+++ leetcode/basic02_stack_and_queue/s04_min_stack.py
@@ -2,20 +2,29 @@
 https://leetcode-cn.com/problems/min-stack/
 leetcode 155 easy
 """
+from basic02_stack_and_queue.my_stacks import Stack
 
 
 class MinStack:
     def __init__(self):
-        pass
+        self._stack = Stack()
+        self._min_stack = Stack()  # 使用辅助栈，来记录当前最小值
 
     def push(self, x: int) -> None:
-        pass
+        self._stack.push(x)
+
+        # min_stack push 当前栈最小值
+        if self._min_stack.peek() is None:
+            self._min_stack.push(x)
+        else:
+            self._min_stack.push(min(x, self._min_stack.peek()))
 
     def pop(self) -> None:
-        pass
+        self._stack.pop()
+        self._min_stack.pop()
 
     def top(self) -> int:
-        pass
+        return self._stack.peek()
 
     def getMin(self) -> int:
-        pass
+        return self._min_stack.peek()
--- leetcode/basic02_stack_and_queue/s05_next_greater_element_i.py
+++ leetcode/basic02_stack_and_queue/s05_next_greater_element_i.py
@@ -4,7 +4,24 @@ leetcode 496 easy
 """
 from typing import List
 
+from basic02_stack_and_queue.my_stacks import Stack
+
 
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        pass
+        """核心：单调栈"""
+        monotonous_stack = Stack()  # 单调栈,单调递减
+        memory_dic = {}
+
+        # 将 nums2 的所有元素，倒序入单调栈
+        for i in range(len(nums2) - 1, -1, -1):
+            # 1.维护单调栈--[将单调栈中 <= 当前元素(nums2[i])的元素，全部 pop 出去]
+            while not monotonous_stack.is_empty() and nums2[i] >= monotonous_stack.peek():
+                monotonous_stack.pop()
+
+            # 2.使用单调栈--[根据单调栈的栈顶元素(stack.peek()),获取值]
+            memory_dic[nums2[i]] = -1 if monotonous_stack.is_empty() else monotonous_stack.peek()
+
+            # 3.元素入栈
+            monotonous_stack.push(nums2[i])
+        return [memory_dic.get(item) for item in nums1]
