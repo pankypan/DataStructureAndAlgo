4b9b777d118ece31d21d79fd3189a2c3cdd7e931
pankypan <1356523334@qq.com>
2020-11-04 22:42:27 +0800

    '11-04'

---
+++ leetcode/basic03_tree/s01_same_tree.py
@@ -0,0 +1,39 @@
+"""
+https://leetcode-cn.com/problems/same-tree/
+leetcode 100 easy
+"""
+# Definition for a binary tree node.
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        """
+        DFS
+        边界条件：
+            验证相同的树，若当前节点都为空，返回true
+            若仅有一个节点为空，说明不相同，返回false
+        递归：
+            对比当前节点的值，进入递归，p的左子树和q的左子树对比，p的右子树和q的右子树对比
+        :param p:
+        :param q:
+        :return:
+        """
+        # 边界条件
+        if p is None and q is None:  # p, q 均为 None
+            return True
+        if p is None or q is None:  # p, q 有一个为 None
+            return False
+
+        # 先比较根节点，再比较左右子树
+        return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+
+if __name__ == '__main__':
+    pass
---
+++ leetcode/basic03_tree/s02_symmetric_tree.py
@@ -0,0 +1,40 @@
+"""
+https://leetcode-cn.com/problems/symmetric-tree/
+leetcode 101 easy
+"""
+# Definition for a binary tree node.
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def dfs(self, root1: TreeNode, root2: TreeNode) -> bool:
+        if root1 is None and root2 is None:
+            return True
+        if root1 is None or root2 is None:
+            return False
+
+        return root1.val == root2.val and self.dfs(root1.left, root2.right) and self.dfs(root1.right, root2.left)
+
+    def isSymmetric(self, root: TreeNode) -> bool:
+        """
+        DFS
+        该问题可以转化为：两个树在什么情况下互为镜像？
+        如果同时满足下面的条件，两个树互为镜像：
+            它们的两个根结点具有相同的值
+            每个树的右子树都与另一个树的左子树镜像对称
+        :param root:
+        :return:
+        """
+        if not root:
+            return True
+        return self.dfs(root.left, root.right)
+
+
+if __name__ == '__main__':
+    pass
