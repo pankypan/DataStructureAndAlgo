4edc3998c09da31f3d7e94e0dbb70830870ab8dc
pankypan <1356523334@qq.com>
2021-02-18 09:40:05 +0800

    '02-18'

--- leetcode/basic01_linked_list/b05_flatten_a_multilevel_doubly_linked_list.py
+++ leetcode/basic01_linked_list/b05_flatten_a_multilevel_doubly_linked_list.py
@@ -14,4 +14,27 @@ class Node:
 
 class Solution:
     def flatten(self, head: Node) -> Node:
-        pass
+        if not head:
+            return head
+
+        # pseudo head to ensure the `prev` pointer is never none
+        pseudo_head = Node(None, None, head, None)
+        self.flatten_dfs(pseudo_head, head)
+
+        # detach the pseudo head from the real head
+        pseudo_head.next.prev = None
+        return pseudo_head.next
+
+    def flatten_dfs(self, prev, curr):
+        """ return the tail of the flatten list """
+        if not curr:
+            return prev
+
+        curr.prev = prev
+        prev.next = curr
+
+        # the curr.next would be tempered in the recursive function
+        temp_next = curr.next
+        tail = self.flatten_dfs(curr, curr.child)
+        curr.child = None
+        return self.flatten_dfs(tail, temp_next)
--- leetcode/basic01_linked_list/c01_reverse_nodes_in_k_group.py
+++ leetcode/basic01_linked_list/c01_reverse_nodes_in_k_group.py
@@ -6,8 +6,38 @@ from basic01_linked_list.common_functions import ListNode
 
 
 class Solution:
+    def reverse(self, head: ListNode, tail: ListNode):
+        prev = tail.next
+        p = head
+
+        while prev != tail:
+            nex = p.next
+            p.next = prev
+
+            prev = p
+            p = nex
+        return tail, head
+
     def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
-        pass
+        dump = ListNode(0)
+        dump.next = head
+        pre = dump
+
+        while head:
+            tail = pre
+            # 查看剩余部分长度是否大于等于 k
+            for i in range(k):
+                tail = tail.next
+                if not tail:
+                    return dump.next
+            nex = tail.next
+            head, tail = self.reverse(head, tail)
+            # 把子链表重新接回原链表
+            pre.next = head
+            tail.next = nex
+            pre = tail
+            head = tail.next
+        return dump.next
 
 
 if __name__ == "__main__":
